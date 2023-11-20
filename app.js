a8cf6efe186c2854fa098358da1c9f708bebd37e
活下去输出 <13234043+live-and-output@user.noreply.gitee.com>
2023-11-20 16:21:53 +0800

    完成cors中间件引入

---
+++ app.js
@@ -0,0 +1,16 @@
+// 导入 express 模块
+const express = require('express')
+// 创建 express 的服务器实例
+const app = express();
+
+//跨域服务，导入cors中间件
+const cors = require('cors')
+//使用cors中间件
+app.use(cors())
+
+app.use(express.urlencoded({ extended: false }))
+
+//启动服务器
+const server=app.listen(8080, () =>{
+    console.log('服务器启动成功，端口号为  http://127.0.0.1:8080')
+})
