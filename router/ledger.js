259eaf48793541ebbdfc594f02f1a1c39d213b7c
活下去输出 <13234043+live-and-output@user.noreply.gitee.com>
2023-12-03 22:31:55 +0800

    发布文章

--- router/ledger.js
+++ router/ledger.js
@@ -2,7 +2,23 @@ const express = require('express');
 const router = express.Router();
 const ledger_Handler = require('../router_handler/ledger');
 
+//文章
+router.get('/getLedger', ledger_Handler.getLedger);
+router.post('/publishLedger', ledger_Handler.publishLedger)
+//评论
+router.get('/getComments', ledger_Handler.getLComments)
+router.post('/publishComment', ledger_Handler.publishComment)
+//点赞
+router.get('/getLikes', ledger_Handler.getLikes)
+router.post('/publishLike', ledger_Handler.publishLike)
+//收藏
+router.get('/getFavorites', ledger_Handler.getFavorites)
+router.post('/publishFavorites', ledger_Handler.publishFavorites)
+//回复
+router.get('/getReply', ledger_Handler.getReply)
+router.post('/publishReply', ledger_Handler.publishReply)
 
-router.get('/detail', ledger_Handler.getLedger);
-
+//回复·点赞
+router.get('/getReplyLikes', ledger_Handler.getReplyLikes)
+router.post('/publishReplyLike', ledger_Handler.publishReplyLike)
 module.exports = router
--- router_handler/ledger.js
+++ router_handler/ledger.js
@@ -4,11 +4,18 @@ const db = require('../db/index');
 
 // 创建文章
 exports.publishLedger=(req, res) => {
-    const { title, content } = req.body;
-    const sql = 'INSERT INTO articles (title, content) VALUES (?, ?)';
-    db.query(sql, [title, content], (err, result) => {
-        if (err) throw err;
-        res.json({ id: result.insertId, title, content });
+    const { title, content, image_url ,user_id} = req.body;
+    const sql = 'INSERT INTO posts (title, content,image_url,user_id)  VALUES (?, ?, ?, ?) ';
+    db.query(sql, [title, content, image_url, user_id], (err, result) => {
+        if (err) {
+            // 执行sql语句失败
+            return res.send({ status: 1, message: err.message })
+        }
+        //执行sql语句成功
+        // 返回任务列表
+        if (result.length === 0) return res.send({ status: 1, message: '添加失败，请稍后再试' })
+        // 查询成功
+        res.send({ status: 0, message: '添加成功'})
     });
 };
 
@@ -30,55 +37,156 @@ exports.getLedger = (req, res) => {
 
 // 发布评论
 exports.publishComment=(req, res) => {
-    const { content, articleId } = req.body;
-    const sql = 'INSERT INTO comments (content, articleId) VALUES (?, ?)';
-    db.query(sql, [content, articleId], (err, result) => {
-        if (err) throw err;
-        res.json({ id: result.insertId, content, articleId });
+    const { content, post_id, user_id } = req.body;
+    const sql = 'INSERT INTO comments (content, post_id ,user_id) VALUES (?, ?, ?)';
+    db.query(sql, [content, post_id, user_id], (err, result) => {
+        if (err) {
+            // 执行sql语句失败
+            return res.send({ status: 1, message: err.message })
+        }
+        //执行sql语句成功
+        // 返回任务列表
+        if (result.length === 0) return res.send({ status: 1, message: '添加失败，请稍后再试' })
+        // 查询成功
+        res.send({ status: 0, message: '添加成功' })
     });
 };
-
-
 
 // 获取评论列表
 exports.getLComments=(req, res) => {
     const sql = 'SELECT * FROM comments';
-    db.query(sql, (err, results) => {
-        if (err) throw err;
-        res.json(results);
+    db.query(sql, (err, result) => {
+        if (err) {
+            // 执行sql语句失败
+            return res.send({ status: 1, message: err.message })
+        }
+        //执行sql语句成功
+        // 返回任务列表
+        if (result.length === 0) return res.send({ status: 1, message: '查询任务信息失败，请稍后再试' })
+        // 查询成功
+        res.send({ status: 0, message: '查询任务信息成功', data: result })
     });
 };
+
 // 发布点赞
 exports.publishLike=(req, res) => {
-    const { articleId } = req.body;
-    const sql = 'INSERT INTO likes (articleId) VALUES (?)';
-    db.query(sql, [articleId], (err, result) => {
-        if (err) throw err;
-        res.json({ id: result.insertId, articleId });
+    const { post_id,user_id } = req.body;
+    const sql = 'INSERT INTO likes (post_id,user_id) VALUES (?, ?)';
+    db.query(sql, [post_id, user_id], (err, result) => {
+        if (err) {
+            // 执行sql语句失败
+            return res.send({ status: 1, message: err.message })
+        }
+        //执行sql语句成功
+        // 返回任务列表
+        if (result.length === 0) return res.send({ status: 1, message: '添加失败，请稍后再试' })
+        // 查询成功
+        res.send({ status: 0, message: '添加成功' })
     });
 }
 // 获取点赞列表
-exports.getLlikes=(req, res) => {
+exports.getLikes=(req, res) => {
     const sql = 'SELECT * FROM likes';
-    db.query(sql, (err, results) => {
-        if (err) throw err;
-        res.json(results);
+    db.query(sql, (err, result) => {
+        if (err) {
+            // 执行sql语句失败
+            return res.send({ status: 1, message: err.message })
+        }
+        //执行sql语句成功
+        // 返回任务列表
+        if (result.length === 0) return res.send({ status: 1, message: '查询任务信息失败，请稍后再试' })
+        // 查询成功
+        res.send({ status: 0, message: '查询任务信息成功', data: result })
     });
 }
 // 发布收藏
-exports.publishCollect=(req, res) => {
-    const { articleId } = req.body;
-    const sql = 'INSERT INTO collects (articleId) VALUES (?)';
-    db.query(sql, [articleId], (err, result) => {
-        if (err) throw err;
-        res.json({ id: result.insertId, articleId });
+exports.publishFavorites=(req, res) => {
+    const { post_id,user_id } = req.body;
+    const sql = 'INSERT INTO Favorites ( post_id,user_id) VALUES (?,?)';
+    db.query(sql, [post_id, user_id], (err, result) => {
+        if (err) {
+            // 执行sql语句失败
+            return res.send({ status: 1, message: err.message })
+        }
+        //执行sql语句成功
+        // 返回任务列表
+        if (result.length === 0) return res.send({ status: 1, message: '添加失败，请稍后再试' })
+        // 查询成功
+        res.send({ status: 0, message: '添加成功' })
     })
 }
 // 获取收藏列表
-exports.getLcollects=(req, res) => {
-    const sql = 'SELECT * FROM collects';
-    db.query(sql, (err, results) => {
-        if (err) throw err;
-        res.json(results);
+exports.getFavorites=(req, res) => {
+    const sql = 'SELECT * FROM Favorites';
+    db.query(sql, (err, result) => {
+        if (err) {
+            // 执行sql语句失败
+            return res.send({ status: 1, message: err.message })
+        }
+        //执行sql语句成功
+        // 返回任务列表
+        if (result.length === 0) return res.send({ status: 1, message: '查询任务信息失败，请稍后再试' })
+        // 查询成功
+        res.send({ status: 0, message: '查询任务信息成功', data: result })
     });
+}
+
+//发表回复
+exports.publishReply=(req, res) => {
+    const { content, post_id, user_id } = req.body;
+    const sql = 'INSERT INTO replies (content, post_id,user_id) VALUES (?,?,?)';
+    db.query(sql, [content, post_id, user_id], (err, result) => {
+        if (err) {  
+            // 执行sql语句失败
+            return res.send({ status: 1, message: err.message })
+        }
+        //执行sql语句成功
+        // 返回任务列表
+        if (result.length === 0) return res.send({ status: 1, message: '添加失败，请稍后再试' })
+        // 查询成功
+        res.send({ status: 0, message: '添加成功' })
+    })
+}
+// 获取回复列表
+exports.getReply=(req, res) => {
+    const sql = 'SELECT * FROM replies';
+    db.query(sql, (err, result) => {
+        if (err) {
+            // 执行sql语句失败
+            return res.send({ status: 1, message: err.message })
+        }
+        //执行sql语句成功
+        // 返回任务列表
+        if (result.length === 0) return res.send({ status: 1, message: '查询任务信息失败，请稍后再试' })
+        // 查询成功
+        res.send({ status: 0, message: '查询任务信息成功', data: result })
+    })
+}
+
+//回复点赞
+exports.publishReplyLike=(req, res) => {
+    const { reply_id,user_id } = req.body;
+    const sql = 'INSERT INTO reply_likes (reply_id,user_id) VALUES (?,?)';
+    db.query(sql, [reply_id, user_id], (err, result) => {
+        if (err) {  
+            return res.send({ status: 1, message: err.message })
+
+        }
+        if (result.length === 0) return res.send({ status: 1, message: '添加失败，请稍后再试' })
+        res.send({ status: 0, message: '添加成功' })
+    })
+}
+//获取回复点赞
+exports.getReplyLikes=(req, res) => {
+    const sql = 'SELECT * FROM reply_likes';
+    db.query(sql, (err, result) => {
+        if (err) {
+            // 执行sql语句失败
+            return res.send({ status: 1, message: err.message })
+        }
+        //执行sql语句成功
+        // 返回任务列表
+        if (result.length === 0) return res.send({ status: 1, message: '查询任务信息失败，请稍后再试' })
+        res.send({ status: 0, message: '查询任务信息成功', data: result })
+    })
 }
